232e19dc3bb7a25fe71b3d90a7dabe52fb525d20
Makombe93 <mak20002@byui.edu>
2023-11-09 13:37:57 +0200

    Commit

--- prepare/Learning02/Resume.cs
+++ prepare/Learning02/Resume.cs
@@ -1,15 +1,21 @@
+using System;
+
 public class Resume
 {
-    public string _personName;
+    public string _name;
+
+    // Make sure to initialize your list to a new List before you use it.
     public List<Job> _jobs = new List<Job>();
-    internal string _name;
 
     public void Display()
     {
-        Console.WriteLine($"Name: {_personName}");
+        Console.WriteLine($"Name: {_name}");
         Console.WriteLine("Jobs:");
+
+        // Notice the use of the custom data type "Job" in this loop
         foreach (Job job in _jobs)
         {
+            // This calls the Display method on each job
             job.Display();
         }
     }
