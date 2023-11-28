802e77cf879e51efc80983233c7c1ef0bcc69ac2
Makombe93 <mak20002@byui.edu>
2023-11-28 14:54:00 +0200

    Commit

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,16 @@
 using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        // Test MathAssignment
+        MathAssignment mathAssignment = new MathAssignment("Batsirai Makombe", "Fractions", "7.3", "8-19");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        // Test WritingAssignment
+        WritingAssignment writingAssignment = new WritingAssignment("Mary Moyo", "European History", "The Causes of World War II");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
